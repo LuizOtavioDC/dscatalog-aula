07ab8e294811e73563d5bdabefb3a11e82d0fb9b
LuizOtavioDC <luiz.otavio.dc@hotmail.com>
2022-02-08 15:35:03 -0300

    custom constraint validator

--- backend/src/main/java/com/luiz/bootcamp/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/luiz/bootcamp/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.luiz.bootcamp.dscatalog.dto;
 
+import com.luiz.bootcamp.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO{
 
 	private static final long serialVersionUID = 1L;
